6a4f4c7c40c29f7bd376da8230fb41abfe0fc706
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2023-10-06 20:06:58 +0300

    Newkom

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@ int main(){
    std::string name;
    std::cout<<"Введите ваше имя"//enter the user name from the keyboard
    std::cin>>name;
-   std::cout<<"Hello world "<<name<<endl;//Output Hello world with the user name
+   std::cout<<"Hello world "<<name<<endl;//Output Hello world with the username
    return 0;
 }
