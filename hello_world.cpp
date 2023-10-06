69536de4ff022cc55b3d93747aae91db314abb22
KireevDanil <kireevdanil974@gmail.com>
2023-10-06 20:05:04 +0300

    Изменил code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
-int main(){ 
-   std::string name;
-   std::cout<<"Введите ваше имя"//Ввыоди имя пользователя с клавиатуры
-   std::cin>>name;
-   std::cout<<"Hello world "<<name<<endl;//Выводим Hello world с именени пользователя
-   return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Введите ваше имя" //Ввыоди имя пользователя с клавиатуры
+      std::cin >>
+    name;
+  std::cout << "Hello world " << name
+            << endl; //Выводим Hello world с именени пользователя
+  return 0;
 }
