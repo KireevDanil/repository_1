7e483b5d80e60d560a0732c9016277ff1dc8c7a8
KireevDanil <146985639+KireevDanil@users.noreply.github.com>
2023-10-06 21:13:47 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@ int main(){
    std::string name;
    std::cout<<"Введите ваше имя"//enter the user name from the keyboard
    std::cin>>name;
-   std::cout<<"Hello world "<<name<<endl;//Output Hello world with the username
+   std::cout<<"Hello world "<<name<<endl;//Output Hello world with the user name
    return 0;
 }
